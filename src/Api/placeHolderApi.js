5352cf94f5352105964acd5196c6978a0ca415e3
chinosalva <ericksalvadorhernandez44@gmail.com>
2022-07-26 09:39:17 -0500

    Ya pude obtener la API

--- src/Api/placeHolderApi.js
+++ src/Api/placeHolderApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BaseURL = 'https://ecomerce-master.herokuapp.com/api/v1/item'
+const BaseURL = 'https://ecomerce-master.herokuapp.com/api/v1'
 
 export const placeHolderApi = axios.create({baseURL:BaseURL})
 
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,11 +1,10 @@
-import axios from "axios";
 import React, {useState, useEffect} from 'react'
-//import {placeHolderApi} from '../Api/placeHolderApi'
+import { placeHolderApi } from '../Api/placeHolderApi'
 
 export const Products = () => {
     const [product, setProduct] = useState([])
     const getProductData = async() => {
-        const res = await axios.get ('https://ecomerce-master.herokuapp.com/api/v1/item')
+        const res = await placeHolderApi.get ('/item')
         console.log(res.data)
         setProduct(res.data)
     }
@@ -18,11 +17,19 @@ export const Products = () => {
     
     return (
         <>
+            <ul>
             {
-                Products.map(product => {
-
-                })
+                product.map(user => (
+                    <li key={user.id} >
+                        <p>
+                            {
+                                `${user.category}`
+                            }
+                        </p>
+                    </li>
+                ))
             }
+            </ul>   
         </>
     )
-}
+    }
